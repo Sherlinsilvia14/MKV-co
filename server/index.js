21f17a8b20806b594647a92da66bc2889d8d19df
Sherlin Silvia <sherlinsilvia14@gmail.com>
2026-01-31 14:26:37 +0530

    Configure for Render Deployment: Serving static files and adding start script

--- server/index.js
+++ server/index.js
@@ -174,5 +174,12 @@ app.get('/api/products/:id', async (req, res) => {
     }
 });
 
-const PORT = 5000;
+// Serve static assets in production
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
